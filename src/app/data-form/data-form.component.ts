2aadab0380c37bff3b498b3aac1537e0c1dc0d21
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-07-03 09:43:30 -0300

    Add: botão resetar

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -39,7 +39,16 @@ export class DataFormComponent implements OnInit{
     // console.log(this.formulario.value);
     this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value)).pipe(
       map(res => res)
-    ).subscribe(dados => console.log(dados));
+    ).subscribe(dados => {
+        console.log(dados);
+        this.resetar();
+      },
+      (error: any) => alert('erro')
+    );
   }
+
+  resetar() {
+    this.formulario.reset();
+    }
 
 }
